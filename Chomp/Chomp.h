f967a6dcc5de51aabaaec630299e764f27524055
John Pope <jp@bellgeorge.com>
2015-01-18 19:35:18 +1100

    Initial Commit

---
+++ Chomp/Chomp.h
@@ -0,0 +1,19 @@
+//
+//  Chomp.h
+//  Chomp
+//
+//  Created by John Pope on 18/01/2015.
+//  Copyright (c) 2015 Mike Ash. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Chomp.
+FOUNDATION_EXPORT double ChompVersionNumber;
+
+//! Project version string for Chomp.
+FOUNDATION_EXPORT const unsigned char ChompVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chomp/PublicHeader.h>
+
+
